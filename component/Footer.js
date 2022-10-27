d8dd398068ca53a9ae620515874951aa4763b13d
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-10-27 23:42:35 +0700

    footer update

--- component/Footer.js
+++ component/Footer.js
@@ -10,41 +10,53 @@ import email from "../public/email.svg";
 
 export default function Footer() {
   return (
-    <div className={styles.footerbox}>
-      <div>
-        <div className={styles.leftbox}>
-          <Image src={logo} alt="SeLogo" />
-          <p>King Mongkut's Institute of Technology Ladkrabang</p>
-          <a>
-            <Image src={facebook} alt="facebook" />
-          </a>
-          <a>
-            <Image src={instagram} alt="facebook" />
-          </a>
-          <a>
-            <Image src={twitter} alt="facebook" />
-          </a>
+    <div className={styles.footerbackground}>
+      <div className={styles.footerbox}>
+        <div className={styles.footerinfo}>
+          <div className={styles.leftbox}>
+            <div className={styles.box}>
+              <div className={styles.logo}>
+                <Image src={logo} alt="SeLogo" />
+              </div>
+              <p className={styles.footerP}>
+                King Mongkut's Institute of Technology Ladkrabang
+              </p>
+              <div className={styles.iconlink}>
+                <a className={styles.link}>
+                  <Image src={facebook} alt="facebook" />
+                </a>
+                <a className={styles.link}>
+                  <Image src={instagram} alt="facebook" />
+                </a>
+                <a className={styles.link}>
+                  <Image src={twitter} alt="facebook" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.rightbox}>
+            <div className={styles.box}>
+              <p className={styles.contactP}>CONTACT US</p>
+              <div className={styles.row}>
+                <Image src={location} alt="location" />
+                <p className={styles.contactinfoP}>ECC704</p>
+              </div>
+              <div className={styles.row}>
+                <Image src={phone} alt="phone" />
+                <p className={styles.contactinfoP}>000-1111111</p>
+              </div>
+              <div className={styles.row}>
+                <Image src={email} alt="email" />
+                <p className={styles.contactinfoP}>se@email.com</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.rightbox}>
-          <p>CONTACT US</p>
-          <div className={styles.row}>
-            <Image src={location} alt="location" />
-            <p>ECC704</p>
-          </div>
-          <div className={styles.row}>
-            <Image src={phone} alt="phone" />
-            <p>000-1111111</p>
-          </div>
-          <div className={styles.row}>
-            <Image src={email} alt="email" />
-            <p>se@email.com</p>
-          </div>
-        </div>
+        <p className={styles.footerP}>
+          © 2022 INTERNATIONAL COLLEGE, KING MONGKUT'S INSTITUTE OF TECHNOLOGY
+          LADKRABANG
+        </p>
       </div>
-      <p>
-        © 2022 INTERNATIONAL COLLEGE, KING MONGKUT'S INSTITUTE OF TECHNOLOGY
-        LADKRABANG
-      </p>
     </div>
   );
 }
