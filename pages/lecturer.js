192f8a99a009f7780527a708643ff0fe83a6c89c
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-10-31 22:24:42 +0700

    new branch and lecturer page draft

---
+++ pages/lecturer.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Lecturer.module.css";
+
+export default function lecturer() {
+  return (
+    <div>
+      <p>In progress...</p>
+      <div className={styles.hero}>
+        <div className={styles.heading}>
+          <h1 className={styles.h1}>Lecturers</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
