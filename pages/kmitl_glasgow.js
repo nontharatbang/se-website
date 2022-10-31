63f9c8339da3da3d521fe1a25880e41b5f098ea8
Athicha Popa <athichaneo@gmail.com>
2022-10-31 20:22:40 +0700

    glasgow

--- pages/kmitl_glasgow.js
+++ pages/kmitl_glasgow.js
@@ -2,7 +2,6 @@ import styles from "../styles/Glasgow.module.css";
 import Image from "next/image";
 import glasgow_icon from "../public/glasgow_icon.svg";
 import Link from "next/Link";
-import Collaps from "../component/Collaps";
 
 export default function curriculum() {
   return (
@@ -127,86 +126,6 @@ export default function curriculum() {
               </Link>
             </p>
           </div>
-        </div>
-        <div className={styles.collapscontainer}>
-          <Collaps label="Application Guidance">
-            <p className={styles.p}>
-              On the UCAS website above, choose Undergraduate 20xx entry, where
-              20xx is the year you are planning to enter UoG, and then select
-              "Apply". This should lead you to the application system. You first
-              need toregister. Make sure you provide an email address that you
-              check regularly (could be @kmitl.ac.th address or your personal
-              email address). Your given names and your last name that you enter
-              when registering should be spelled exactly as written in your
-              passport. If there is any field in the registration form that is
-              unclear, click on ? or "Help" to see an explanation. After you
-              have registered, you will receive an email from UCAS asking you to
-              verify your email address. You should follow the instruction in
-              the email. After you have registered, you can then login to start
-              filling in your application. When asked how you are applying, you
-              should specify that you apply as "individual" (thus a "buzzword"
-              is not required). When asked which program to apply, choose BSc
-              (Hons) Software Engineering program at the University of Glasgow
-              (UCAS G430) for entry into Year 3 of the program. You must include
-              a short personal statement. The statement should include your
-              reason for choosing software engineering as your program of study,
-              your reason for choosing the KMITL-Glasgow Double-Degree Program
-              in Software Engineering, and your future plan after your graduate.
-              Reference letters are not needed. When you submit your
-              application, you will be asked to pay theapplication fee using a
-              credit card. You can ask someone else to pay for you using their
-              credit card. If the credit card you use does not work, try another
-              one. After you have submitted your UCAS application, notify the SE
-              program director that your have done so within 30 June. Also send
-              a copy of your IELTS score report to the program director. The
-              program director will forward your IELTS score report together
-              with your transcript to the admissions office at UoG. There is no
-              need to send us your transcript as we can obtain it by ourselves.
-              If you satisfy all the requirements, UoG will notify you with
-              anunconditional offer via email. If not, UoG will send you
-              aconditional offer via email, detailing the conditions that you
-              need to satisfy and the deadline (e.g. IELTS score or final GPA
-              for Year 2 at KMITL). You must accept the offer within the
-              deadline stated in your offer. Once you have been given and
-              accepted an unconditional offer, UoG will start preparing aCAS
-              statement for you. UoG will send you adraft of your CAS statement
-              for you to check for correctness. Your CAS statement is a summary
-              of your personal information, educational records, and your
-              program of study in the UK. The CAS statement will be stored in an
-              online server accessible by UK universities and the UK immigration
-              department (called the Home Office). Your CAS statement will be
-              read by the visa staff when you apply for a visa. You should check
-              that all the information is correct and, in particular, thetuition
-              fee listed in the statement is the 20%-discounted rate. Inform UoG
-              if any correction is needed. Once you have confirmed that the
-              draft of your CAS statement is correct, UoG will email you a copy
-              of your final CAS statement. You should print it out and later
-              include it with your visa application. During the same time after
-              you accepted the unconditional offer, you will receive some
-              information from UoG's student services regarding the
-              registration, student accommodation, internet account, etc via
-              email. You can just response to their request. When you are asked
-              to register for courses in the upcoming semester, you can ignore
-              that. You are to do thecourse registration when you are in the UK.
-              Your advisor at UoG will help you with the course registration
-              when you arrive at UoG. Regarding the accommodation, UoG will send
-              you an application form for applying for auniversity
-              accommodation. There is a wide range of options. You can also
-              apply for a private student accommodation. For the latter, you can
-              do so by yourself on the web any time (even before you receive an
-              offer). You can consult the SE students currently studying at
-              Glasgow for advice accommodations. For a private accommodation,
-              you could apply even before you receive an offer from UoG. After
-              your have received a copy of your final CAS statement in Step 10
-              above, you can start applying for your UK visa. See below for the
-              visa application guidance. You should plan to arrive at Glasgow at
-              the beginning of the orientation week, which is typically on the
-              Monday in the middle of September, or before that. If you have any
-              question or problem on the KMITL-Glasgow Double-Degree Program in
-              Software Engineering, please contact the SE program director,
-              Asst.Prof.Dr. Visit Hirankitti (visit.hi@kmitl.ac.th).
-            </p>
-          </Collaps>
         </div>
       </div>
     </div>
