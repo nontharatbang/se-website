9b0c59ee7f7c1d7b4cf5f1cb9bc1821232a973f5
Athicha Popa <athichaneo@gmail.com>
2022-12-18 13:57:24 +0700

    fix cursor to pointer cursor in program page

--- pages/program.js
+++ pages/program.js
@@ -26,10 +26,12 @@ export default function program() {
                 <div className="px-5 py-2 flex justify-between">
                   <div>
                     <Link href="/curriculum">
-                      <p className="pt-1 text-2xl uppercase">curriculum</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        curriculum
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/curriculum">
                       <span className="self-center pl-2 whitespace-nowrap">
@@ -47,10 +49,12 @@ export default function program() {
                 <div className="px-5 py-2 justify-between">
                   <div>
                     <Link href="/kmitl_glasgow">
-                      <p className="pt-1 text-2xl uppercase">kmitl x glasgow</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        kmitl x glasgow
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/kmitl_glasgow">
                       <span className="self-center pl-2 whitespace-nowrap">
@@ -70,10 +74,12 @@ export default function program() {
                 <div className="px-5 py-2 justify-between">
                   <div>
                     <Link href="/study_plan">
-                      <p className="pt-1 text-2xl uppercase">Study plan</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        Study plan
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/study_plan">
                       <span className="self-center pl-2 whitespace-nowrap">
@@ -91,10 +97,12 @@ export default function program() {
                 <div className="px-5 py-2 flex justify-between">
                   <div>
                     <Link href="/internship">
-                      <p className="pt-1 text-2xl uppercase">Internship</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        Internship
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/internship">
                       <span className="self-center pl-2 whitespace-nowrap">
@@ -114,10 +122,12 @@ export default function program() {
                 <div className="px-5 py-2 flex justify-between">
                   <div>
                     <Link href="/study_abroad">
-                      <p className="pt-1 text-2xl uppercase">study abroad</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        study abroad
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/study_abroad">
                       <span className="self-center pl-2 whitespace-nowrap">
@@ -135,10 +145,12 @@ export default function program() {
                 <div className="px-5 py-2 justify-between">
                   <div>
                     <Link href="/lecturer">
-                      <p className="pt-1 text-2xl uppercase">lecturer</p>
+                      <p className="pt-1 text-2xl uppercase cursor-pointer">
+                        lecturer
+                      </p>
                     </Link>
                   </div>
-                  <div className="flex flex-wrap py-2">
+                  <div className="flex flex-wrap py-2 cursor-pointer">
                     <Image src={learnmore_arrow} alt="learnmore arrow" />
                     <Link href="/lecturer">
                       <span className="self-center pl-2 whitespace-nowrap">
