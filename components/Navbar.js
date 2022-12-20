d755943a0da0373e9cdc2fb291100dbed1a1bd16
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-12-21 01:50:24 +0700

    add cursor-pointer

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,22 +6,22 @@ export default function Navbar() {
   return (
     <nav className="bg-white h-28 w-full px-10 flex flex-col justify-center z-auto fixed z-20 top-0 left-0 border-b border-gray-200 ">
       <div className="flex justify-between items-center">
-        <div>
+        <div className="cursor-pointer">
           <Link href="/">
             <Image src={logo} alt="SeLogo" />
           </Link>
         </div>
         <ul className="grid grid-cols-4 gap-x-8 justify-end items-center text-center text-black font-bold">
-          <li>
+          <li className="cursor-pointer">
             <Link href="/about">About</Link>
           </li>
-          <li>
+          <li className="cursor-pointer">
             <Link href="/program">Program</Link>
           </li>
-          <li>
+          <li className="cursor-pointer">
             <Link href="/admission">Admission</Link>
           </li>
-          <li>
+          <li className="cursor-pointer">
             <Link href="/news/0">News</Link>
           </li>
         </ul>
